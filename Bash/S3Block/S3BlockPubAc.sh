e899086c4bce1936b8f348ee9464c2bf5e7dce49
João Avelin <67546769+TheK6@users.noreply.github.com>
2024-11-16 11:58:11 -0300

    Update S3BlockPubAc.sh

--- Bash/S3Block/S3BlockPubAc.sh
+++ Bash/S3Block/S3BlockPubAc.sh
@@ -2,28 +2,7 @@
 
 # List of S3 bucket names
 buckets=(
-"codescan-prod-snapshot"
-"codescanng-logs"
-"cf-templates-kruoe3r1jkg0-us-east-1"
-"codescanng-logs-elb-test-internal"
-"elasticbeanstalk-us-east-1-567829905188"
-"97ac76498cee49b8bebeb1e33f3efc23-logs"
-"codescanng-cache-ohio"
-"downloads.code-scan.com.logs"
-"cf-templates-kruoe3r1jkg0-eu-central-1"
-"muralitry11"
-"downloads.code-scan.com.bucket"
-"codescanng-cache-ap"
-"codescanng-cache-eu"
-"storequeryoutput"
-"poc-preview-upload"
-"codescanng-logs-elb-test"
-"codepipeline-us-east-1-673025056697"
-"hardening-scripts-codescan"
-"codescanng-build"
-"codescanng-cache"
-"rackspace-db4ac94b8709a0357e43cdbe0725c3b77a4833ca"
-"codescanawsdocumentation"
+
 )
 
 # Count of buckets
